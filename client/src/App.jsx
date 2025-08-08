24f80b5e0e1b674f6d090fa6147627e7090a35f3
Rohit Patidar <rohitpatidar583@gmail.com>
2025-08-08 12:19:30 +0530

    complete the header section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,11 +3,13 @@ import Home from './pages/Home'
 import SignIn from './pages/SignIn'
 import SignOut from './pages/SignOut'
 import About from './pages/About'
+import Header from './components/Header'
 
 function App() {
 
   return (
     <BrowserRouter>
+    <Header/>
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/about' element={<About/>} />
